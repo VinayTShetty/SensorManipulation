63a630c3de05ad6df8d12db9903133d4ded13197
VinayTS <vinay@succorfish.com>
2021-02-01 15:14:01 +0530

    Packet Refracition is in Progress as it was a error while fetching packets.

--- app/src/main/java/com/example/utilityproject/UtilityClass.java
+++ app/src/main/java/com/example/utilityproject/UtilityClass.java
@@ -12,11 +12,12 @@ public class UtilityClass {
     }
 
     public static String getOnlySensorDataPackets(String hexValueInput){
-        String getLengthToExtract=hexValueInput.substring(0,2);
+        String getLengthToExtract=hexValueInput.substring(2,4);
         int convertedHexValue=Integer.parseInt(getLengthToExtract,16);
-        convertedHexValue=convertedHexValue-3;
-        System.out.println(" Value Returened "+hexValueInput.substring(10,convertedHexValue+10));
-        return hexValueInput.substring(10,convertedHexValue+10);
+        convertedHexValue=convertedHexValue-1;
+        convertedHexValue=convertedHexValue*2;
+        System.out.println(" Value Returened "+hexValueInput.substring(6,convertedHexValue+6));
+       return hexValueInput.substring(6,convertedHexValue+6);
 
     }
 }

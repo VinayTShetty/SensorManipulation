f02de06b7f793f67a69c0c1286199627902caa2a
VinayTS <vinay@succorfish.com>
2021-03-17 11:22:58 +0530

    Getting the Date From TimeStamp is completed. Mehtod Used=getDateFromTimeStamp();

--- app/src/main/java/com/example/utilityproject/MainActivity.java
+++ app/src/main/java/com/example/utilityproject/MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.utilityproject;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.content.Intent;
 import android.icu.text.SimpleDateFormat;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,31 +23,33 @@ import org.w3c.dom.Text;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 import java.util.TimeZone;
 
-public class MainActivity extends AppCompatActivity   {
+public class MainActivity extends AppCompatActivity {
     Button timeset;
     TextView timesetTimeStampView;
     private int mYear, mMonth, mDay, mHour, mMinute;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        timeset=(Button)findViewById(R.id.buttonCLick);
-        timesetTimeStampView=(TextView)findViewById(R.id.timeStamp_textView);
+        timeset = (Button) findViewById(R.id.buttonCLick);
+        timesetTimeStampView = (TextView) findViewById(R.id.timeStamp_textView);
         getCurrentDate();
         timeset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             DatePickerDialog datePickerDialog=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
-                 @Override
-                 public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                       mYear=year;
-                       mMonth=month;
-                       mDay=dayOfMonth;
-                     show_TimePickerDialog();
-                 }
-             },mYear,mMonth,mDay);
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        mYear = year;
+                        mMonth = month;
+                        mDay = dayOfMonth;
+                        show_TimePickerDialog();
+                    }
+                }, mYear, mMonth, mDay);
 
 
                 Calendar c1 = Calendar.getInstance();
@@ -54,9 +58,8 @@ public class MainActivity extends AppCompatActivity   {
                 c1.set(Calendar.DAY_OF_WEEK, 1);
 
                 int year1 = c1.get(Calendar.YEAR);
-                int month1 = c1.get(Calendar.MONTH)+1;
+                int month1 = c1.get(Calendar.MONTH) + 1;
                 int day1 = c1.get(Calendar.DAY_OF_MONTH);
-
 
 
                 Calendar c2 = Calendar.getInstance();
@@ -64,10 +67,8 @@ public class MainActivity extends AppCompatActivity   {
                 c2.set(Calendar.DAY_OF_WEEK, 7);
 
                 int year7 = c2.get(Calendar.YEAR);
-                int month7 = c2.get(Calendar.MONTH)+1;
+                int month7 = c2.get(Calendar.MONTH) + 1;
                 int day7 = c2.get(Calendar.DAY_OF_MONTH);
-
-
 
 
                 datePickerDialog.getDatePicker().setMinDate(c1.getTimeInMillis());
@@ -78,22 +79,25 @@ public class MainActivity extends AppCompatActivity   {
 
     }
 
-   private void getCurrentDate(){
-       Calendar c = Calendar.getInstance();
-       mYear = c.get(Calendar.YEAR);
-       mMonth = c.get(Calendar.MONTH);
-       mDay = c.get(Calendar.DAY_OF_MONTH);
-       /**
-        * Picking up time from android.
-        */
-       mHour= c.get(Calendar.HOUR);// 24 HOURS formatt--->HOUR_OF_DAY
-       mMinute=c.get(Calendar.MINUTE);
-   }
+    private void getCurrentDate() {
+        Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+        /**
+         * Picking up time from android.
+         */
+        mHour = c.get(Calendar.HOUR);// 24 HOURS formatt--->HOUR_OF_DAY
+        mMinute = c.get(Calendar.MINUTE);
+    }
 
 
     @Override
     protected void onResume() {
         super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            getDate(1615960068);
+        }
     }
 
     @Override
@@ -107,36 +111,36 @@ public class MainActivity extends AppCompatActivity   {
     }
 
 
-    private void show_TimePickerDialog(){
+    private void show_TimePickerDialog() {
         TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
             @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
-                System.out.println("TimeStamp_Data=  Hours= "+hourOfDay+" Minutes= "+minutes);
-                convert_SelectedDate_Time_TimeStamp(mYear,mMonth,mDay,hourOfDay,minutes);
+                System.out.println("TimeStamp_Data=  Hours= " + hourOfDay + " Minutes= " + minutes);
+                convert_SelectedDate_Time_TimeStamp(mYear, mMonth, mDay, hourOfDay, minutes);
             }
         }, mHour, mMinute, false);
         timePickerDialog.show();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    private long convert_SelectedDate_Time_TimeStamp(int year_input, int month_input, int date_input, int hour_input, int mins_input){
-        long timeStampConvertedData=0;
+    private long convert_SelectedDate_Time_TimeStamp(int year_input, int month_input, int date_input, int hour_input, int mins_input) {
+        long timeStampConvertedData = 0;
         Calendar calendarLocal = new GregorianCalendar(TimeZone.getDefault());
         calendarLocal.set(year_input, month_input, date_input);
         calendarLocal.set(Calendar.HOUR_OF_DAY, hour_input);
         calendarLocal.set(Calendar.MINUTE, mins_input);
-        calendarLocal.set(Calendar.SECOND,0);
-        timeStampConvertedData= calendarLocal.getTimeInMillis();
-        System.out.println("TimeStamp_Data TimeInMilliSeconds= "+timeStampConvertedData);
-        timesetTimeStampView.setText("Time = "+calendarLocal.get(Calendar.HOUR)+":"+calendarLocal.get(Calendar.MINUTE)+"\n"+" TimeStamp = "+timeStampConvertedData);
-        textViewSetDate_Time(timesetTimeStampView,calendarLocal);
+        calendarLocal.set(Calendar.SECOND, 0);
+        timeStampConvertedData = calendarLocal.getTimeInMillis();
+        System.out.println("TimeStamp_Data TimeInMilliSeconds= " + timeStampConvertedData);
+        timesetTimeStampView.setText("Time = " + calendarLocal.get(Calendar.HOUR) + ":" + calendarLocal.get(Calendar.MINUTE) + "\n" + " TimeStamp = " + timeStampConvertedData);
+        textViewSetDate_Time(timesetTimeStampView, calendarLocal);
         return timeStampConvertedData;
     }
 
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    private void textViewSetDate_Time(TextView date_Time, Calendar calendar){
+    private void textViewSetDate_Time(TextView date_Time, Calendar calendar) {
         /**
          * 24 hours Formatt
          */
@@ -158,12 +162,48 @@ public class MainActivity extends AppCompatActivity   {
         /**
          * 12 hours formatt discarding mins.
          */
-       // calendar = Calendar.getInstance();
+        // calendar = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat("hh:mm a");
         String test = sdf.format(calendar.getTime());
         date_Time.setText(test);
         Log.e("TEST", test);
+    }
 
+    //TimeStamp to Time
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private String getTimeFromTimeStamp(){
+      /*  SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        String dateString = formatter.format(new Date(Long.parseLong("1615902512")));
+        timesetTimeStampView.setText("Date----------->"+dateString);*/
+
+        try{
+            Calendar calendar = Calendar.getInstance();
+            TimeZone tz = TimeZone.getDefault();
+            calendar.setTimeInMillis(1615903584 * 1000);
+            calendar.add(Calendar.MILLISECOND, tz.getOffset(calendar.getTimeInMillis()));
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date currenTimeZone = (Date) calendar.getTime();
+            timesetTimeStampView.setText("Curent Time--->"+sdf.format(currenTimeZone));
+            return sdf.format(currenTimeZone);
+        }catch (Exception e) {
+        }
+        return "";
     }
+
+
+    /**
+     *
+     * This method is used to get Date From TimeStamp.
+     *
+     */
+    private String getDateFromTimeStamp(long time) {
+        Calendar cal = Calendar.getInstance(Locale.ENGLISH);
+        cal.setTimeInMillis(time * 1000);
+        String date = DateFormat.format("dd-MM-yyyy", cal).toString();
+        timesetTimeStampView.setText("Curent Time--->"+date);
+        return date;
     }
+}
 
